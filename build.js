c8b02569c880c87ec37aabedb2b430eb73737a95
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-25 19:42:00 +0000

    Deploy dcversus/dcversus.ru to github.com/dcversus/dcversus.ru.git:gh-pages

---
+++ build.js
@@ -0,0 +1,80 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("Object.defineProperty(__webpack_exports__, \"__esModule\", { value: true });\n/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__base_css__ = __webpack_require__(1);\n/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__base_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__base_css__);\n\n\nvar mainText = 'Василиса Версус';\nvar mainFormat = 'color: #f9690e; font-size: 3em';\nvar additionalText = 'https://dcversus.ru/';\n\nconsole.clear();\nconsole.log('%c' + mainText, mainFormat);\nconsole.log(additionalText);\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvaW5kZXguanM/OTU1MiJdLCJuYW1lcyI6WyJtYWluVGV4dCIsIm1haW5Gb3JtYXQiLCJhZGRpdGlvbmFsVGV4dCIsImNvbnNvbGUiLCJjbGVhciIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUFBO0FBQUE7O0FBRUEsSUFBTUEsV0FBVyxpQkFBakI7QUFDQSxJQUFNQyxhQUFhLGdDQUFuQjtBQUNBLElBQU1DLGlCQUFpQixzQkFBdkI7O0FBRUFDLFFBQVFDLEtBQVI7QUFDQUQsUUFBUUUsR0FBUixRQUFpQkwsUUFBakIsRUFBNkJDLFVBQTdCO0FBQ0FFLFFBQVFFLEdBQVIsQ0FBWUgsY0FBWiIsImZpbGUiOiIwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICcuL2Jhc2UuY3NzJztcblxuY29uc3QgbWFpblRleHQgPSAn0JLQsNGB0LjQu9C40YHQsCDQktC10YDRgdGD0YEnO1xuY29uc3QgbWFpbkZvcm1hdCA9ICdjb2xvcjogI2Y5NjkwZTsgZm9udC1zaXplOiAzZW0nO1xuY29uc3QgYWRkaXRpb25hbFRleHQgPSAnaHR0cHM6Ly9kY3ZlcnN1cy5ydS8nO1xuXG5jb25zb2xlLmNsZWFyKCk7XG5jb25zb2xlLmxvZyhgJWMke21haW5UZXh0fWAsIG1haW5Gb3JtYXQpO1xuY29uc29sZS5sb2coYWRkaXRpb25hbFRleHQpO1xuXG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIC4vc3JjL2luZGV4LmpzIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///0\n");
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+eval("// removed by extract-text-webpack-plugin\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvYmFzZS5jc3M/MjZmMyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiIxLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8gcmVtb3ZlZCBieSBleHRyYWN0LXRleHQtd2VicGFjay1wbHVnaW5cblxuXG4vLy8vLy8vLy8vLy8vLy8vLy9cbi8vIFdFQlBBQ0sgRk9PVEVSXG4vLyAuL3NyYy9iYXNlLmNzc1xuLy8gbW9kdWxlIGlkID0gMVxuLy8gbW9kdWxlIGNodW5rcyA9IDAiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///1\n");
+
+/***/ })
+/******/ ]);
